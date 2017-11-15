253d67b1c5e6fb19d491c4bb361a292068ff2238
SaiKishoret99 <saikishoret99@gmail.com>
2017-11-15 16:28:11 +0530

    tut-4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[]  = [
+    { id: 11, name: 'Kishore' },
+    { id: 12, name: 'Faisal' },
+    { id: 13, name: 'Aditya' },
+    { id: 14, name: 'Kartheek' },
+    { id: 15, name: 'Naveen' },
+    { id: 16, name: 'Rahul' },
+    { id: 17, name: 'Sachin' },
+    { id: 18, name: 'Lokesh' },
+    { id: 19, name: 'Ananth' },
+    { id: 20, name: 'Chetan' }
+];
